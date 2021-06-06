08d9ce70062fce3ab90a4dc3efcbf8cae983a0fa
nat-js <Angel_tommy@foxmail.com>
2021-06-06 12:24:29 +0800

    Separate declarations and definitions

--- include/i18n/i18n.h
+++ include/i18n/i18n.h
@@ -14,6 +14,8 @@
 
 class i18n
 {
+  typedef std::unordered_map<std::string, std::string> dictionary;
+
 public:
 
   i18n(const i18n&) = delete;
@@ -21,43 +23,100 @@ public:
   i18n& operator=(const i18n&) = delete;
   i18n& operator=(i18n&&) = delete;
 
-  static void Init(std::filesystem::path localePath = "locales", std::string locale = "en-US", std::string defaultLocale = "en-US", std::string localeExtension = ".locale")
-  {
-    Get().m_localePath = localePath;
-    Get().m_defaultLocale = defaultLocale;
-    Get().m_locale = locale;
-    Get().m_localeExtension = localeExtension;
-    Get().loadDefaultDictionary();
-    SetLocale(locale);
-  }
+  static void Init(std::filesystem::path localePath = "locales", std::string locale = "en-US", std::string defaultLocale = "en-US", std::string localeExtension = ".locale");
 
-  static i18n& Get() { static i18n instance; return instance; }
+  static i18n& Get();
 
-  static void SetLocale(const std::string locale) { Get().ISetLocale(locale); }
+  static void SetLocale(const std::string locale);
   
-  static std::string GetLocale() { return Get().m_locale; }
+  static std::string GetLocale();
 
-  static const std::string Translate(std::string msgid) { return Get().ITranslate(msgid); }
+  static const std::string Translate(std::string msgid);
 
   template<typename... Types>
-  static const std::string Translate(std::string msgid, Types... args) { return Get().ITranslate(msgid, std::forward<Types>(args)...); }
+  static const std::string Translate(std::string msgid, Types... args);
 
 private:
   i18n() {}
   ~i18n() {}
+
+  enum TranlateMode
+  {
+    NORMAL,
+    FORMAT,
+    NAMESPACE,
+    NAMESPACE_FORMAT
+  };
 
   std::filesystem::path m_localePath;
   std::string m_localeExtension;
   bool m_defaultLocale_exists;
   std::string m_defaultLocale;
   std::string m_locale;
-  std::unordered_map<std::string, std::string> m_defaultDictionary;
-  std::unordered_map<std::string, std::string> m_dictionary;
+  dictionary m_defaultDictionary;
+  dictionary m_dictionary;
 
-  void ISetLocale(const std::string locale) { m_locale = locale; loadDictionary(locale); }
+  void ISetLocale(const std::string locale);
 
-  std::string ITranslate(const std::string msgid)
+  template<typename... Types>
+  std::string ITranslate(TranlateMode tm, std::string msgid, Types... args);
+
+  dictionary parseDictionary(std::filesystem::path locale_path);
+
+  void loadDefaultDictionary();
+
+  void loadDictionary(const std::string locale);
+
+  std::filesystem::path getLocalePath(std::string locale);
+};
+
+inline void i18n::Init(std::filesystem::path localePath, std::string locale, std::string defaultLocale, std::string localeExtension)
+{
+  Get().m_localePath = localePath;
+  Get().m_defaultLocale = defaultLocale;
+  Get().m_locale = locale;
+  Get().m_localeExtension = localeExtension;
+  Get().loadDefaultDictionary();
+  SetLocale(locale);
+}
+
+inline i18n& i18n::Get()
+{
+  static i18n instance;
+  return instance;
+}
+
+inline void i18n::SetLocale(const std::string locale)
+{
+  Get().ISetLocale(locale);
+}
+
+inline std::string i18n::GetLocale() {
+  return Get().m_locale;
+}
+
+inline const std::string i18n::Translate(std::string msgid)
+{
+  return Get().ITranslate(NORMAL, msgid);
+}
+
+template<typename... Types>
+inline const std::string i18n::Translate(std::string msgid, Types... args) {
+  return Get().ITranslate(FORMAT, msgid, std::forward<Types>(args)...);
+}
+
+inline void i18n::ISetLocale(const std::string locale)
+{
+  m_locale = locale;
+  loadDictionary(locale);
+}
+
+template<typename... Types>
+inline std::string i18n::ITranslate(TranlateMode tm, std::string msgid, Types... args)
+{
+  switch (tm)
   {
+  case NORMAL:
     if (!m_defaultLocale_exists) // If not using a locale file for the default locale
     {
       if (m_locale == m_defaultLocale) return msgid; // Current locale is the default locale
@@ -68,11 +127,9 @@ private:
     if (m_locale == m_defaultLocale) return m_defaultDictionary[msgid]; // Current locale is the default locale
     if (!m_dictionary.count(msgid)) return m_defaultDictionary[msgid]; // Tranlation doesn't exist in the locale file
     return m_dictionary[msgid]; // Return translated string from the dictionary
-  }
-
-  template<typename... Types>
-  std::string ITranslate(std::string msgid, Types... args)
-  {
+    break;
+  
+  case FORMAT:
     if (!m_defaultLocale_exists) // If not using a locale file for the default locale
     {
       if (m_locale == m_defaultLocale) return i18n_format(msgid, std::forward<Types>(args)...); // Current locale is the default locale
@@ -83,71 +140,72 @@ private:
     if (m_locale == m_defaultLocale) return i18n_format(m_defaultDictionary[msgid], std::forward<Types>(args)...); // Current locale is the default locale
     if (!m_dictionary.count(msgid)) return i18n_format(m_defaultDictionary[msgid], std::forward<Types>(args)...); // Tranlation doesn't exist in the locale file
     return i18n_format(m_dictionary[msgid], std::forward<Types>(args)...); // Return translated string from the dictionary
+    break;
   }
+}
 
-  std::unordered_map<std::string, std::string> parseDictionary(std::filesystem::path locale_path)
+inline i18n::dictionary i18n::parseDictionary(std::filesystem::path locale_path)
+{
+  dictionary dictionary;
+  enum lineType { MSG_ID, MSG_STR };
+  std::string line;
+  std::string prev_line;
+  lineType prev_type;
+  std::ifstream file (locale_path);
+  while (std::getline(file, line))
   {
-    std::unordered_map<std::string, std::string> dictionary;
-    enum lineType { MSG_ID, MSG_STR };
-    std::string line;
-    std::string prev_line;
-    lineType prev_type;
-    std::ifstream file (locale_path);
-    while (std::getline(file, line))
+    if (line.find("msgid:") == 0)
     {
-      if (line.find("msgid:") == 0)
-      {
-        line.erase(0, 6);
-        while (line.find(" ") == 0) line.erase(0, 1);
-        prev_line = line;
-        prev_type = MSG_ID;
-      }
-      if (prev_type == MSG_ID && line.find("msgstr:") == 0)
-      {
-        line.erase(0, 7);
-        while (line.find(" ") == 0) line.erase(0, 1);
-        dictionary[prev_line] = line;
-        prev_line = line;
-        prev_type = MSG_STR;
-      }
+      line.erase(0, 6);
+      while (line.find(" ") == 0) line.erase(0, 1);
+      prev_line = line;
+      prev_type = MSG_ID;
     }
-    file.close();
-    return dictionary;
+    if (prev_type == MSG_ID && line.find("msgstr:") == 0)
+    {
+      line.erase(0, 7);
+      while (line.find(" ") == 0) line.erase(0, 1);
+      dictionary[prev_line] = line;
+      prev_line = line;
+      prev_type = MSG_STR;
+    }
   }
+  file.close();
+  return dictionary;
+}
 
-  void loadDefaultDictionary()
+inline void i18n::loadDefaultDictionary()
+{
+  std::filesystem::path defaultLocale_path = getLocalePath(m_defaultLocale);
+  if (!std::filesystem::exists(defaultLocale_path))
   {
-    std::filesystem::path defaultLocale_path = getLocalePath(m_defaultLocale);
-    if (!std::filesystem::exists(defaultLocale_path))
-    {
-      m_defaultLocale_exists = false;
-      m_defaultDictionary.clear();
-      return;
-    }
-    m_defaultLocale_exists = true;
-    m_defaultDictionary = parseDictionary(defaultLocale_path);
+    m_defaultLocale_exists = false;
+    m_defaultDictionary.clear();
+    return;
   }
+  m_defaultLocale_exists = true;
+  m_defaultDictionary = parseDictionary(defaultLocale_path);
+}
 
-  void loadDictionary(const std::string locale)
+inline void i18n::loadDictionary(const std::string locale)
+{
+  if (locale == m_defaultLocale)
   {
-    if (locale == m_defaultLocale)
-    {
-      m_dictionary.clear();
-      return;
-    }
-    std::filesystem::path locale_path = getLocalePath(locale);
-    if (!std::filesystem::exists(locale_path))
-    {
-      m_locale = m_defaultLocale;
-      m_dictionary.clear();
-      return;
-    }
-    m_dictionary = parseDictionary(locale_path);
+    m_dictionary.clear();
+    return;
   }
+  std::filesystem::path locale_path = getLocalePath(locale);
+  if (!std::filesystem::exists(locale_path))
+  {
+    m_locale = m_defaultLocale;
+    m_dictionary.clear();
+    return;
+  }
+  m_dictionary = parseDictionary(locale_path);
+}
 
-  std::filesystem::path getLocalePath(std::string locale)
-  {
-    std::string locale_filename = locale + m_localeExtension;
-    return (m_localePath / locale_filename);
-  }
-};
+inline std::filesystem::path i18n::getLocalePath(std::string locale)
+{
+  std::string locale_filename = locale + m_localeExtension;
+  return (m_localePath / locale_filename);
+}
